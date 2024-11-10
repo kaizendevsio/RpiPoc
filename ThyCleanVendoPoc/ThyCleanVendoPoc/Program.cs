5555a733b1e9e72c74fbf3b8592df18dbf1a9041
Xeon <xeon@g.com>
2024-11-10 18:55:21 +0800

    LED Test with push button

--- ThyCleanVendoPoc/ThyCleanVendoPoc/Program.cs
+++ ThyCleanVendoPoc/ThyCleanVendoPoc/Program.cs
@@ -1,21 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Device.Gpio;
-using System.Threading;
 
-Console.WriteLine("Starting LED Blink Program");
+Console.WriteLine("Starting Button LED Control Program");
 
-var ledPin = 17; // Use the GPIO pin you’ve connected the LED to
+var ledPin = 17; // GPIO pin for the LED
+var buttonPin = 27; // GPIO pin for the button
 using var controller = new GpioController();
 controller.OpenPin(ledPin, PinMode.Output);
+controller.OpenPin(buttonPin, PinMode.InputPullUp); // Enable internal pull-up resistor
+
+Console.WriteLine("Press the button to turn the LED on. Release to turn it off.");
 
 while (true)
 {
-    Console.WriteLine("Turning LED ON");
-    controller.Write(ledPin, PinValue.High); // LED ON
-    Thread.Sleep(2000); // Wait 2 seconds
+    // Read the state of the button
+    var buttonPressed = controller.Read(buttonPin) == PinValue.Low; // Button is pressed if reading is Low
 
-    Console.WriteLine("Turning LED OFF");
-    controller.Write(ledPin, PinValue.Low); // LED OFF
-    Thread.Sleep(1000); // Wait 1 second
+    if (buttonPressed)
+    {
+        // Button is pressed
+        Console.WriteLine("Button pressed - Turning LED ON");
+        controller.Write(ledPin, PinValue.High); // Turn LED on
+    }
+    else
+    {
+        // Button is released
+        Console.WriteLine("Button released - Turning LED OFF");
+        controller.Write(ledPin, PinValue.Low); // Turn LED off
+    }
+
+    Thread.Sleep(100); // Small delay to debounce the button
 }
