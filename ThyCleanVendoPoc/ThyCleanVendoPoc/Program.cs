ffe7c7f2fe65033f99dd0a301f32adec36da2c4f
Xeon <xeon@g.com>
2024-11-10 18:43:51 +0800

    LED Test

--- ThyCleanVendoPoc/ThyCleanVendoPoc/Program.cs
+++ ThyCleanVendoPoc/ThyCleanVendoPoc/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Device.Gpio;
+using System.Threading;
+
+Console.WriteLine("Starting LED Blink Program");
+
+var ledPin = 17; // Use the GPIO pin you’ve connected the LED to
+using var controller = new GpioController();
+controller.OpenPin(ledPin, PinMode.Output);
+
+while (true)
+{
+    Console.WriteLine("Turning LED ON");
+    controller.Write(ledPin, PinValue.High); // LED ON
+    Thread.Sleep(2000); // Wait 2 seconds
+
+    Console.WriteLine("Turning LED OFF");
+    controller.Write(ledPin, PinValue.Low); // LED OFF
+    Thread.Sleep(1000); // Wait 1 second
+}
